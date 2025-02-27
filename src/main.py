748979b372c1ea0ce238d24784a080e8bd890d88
tiankongzhise <104021016+tiankongzhise@users.noreply.github.com>
2025-02-27 17:52:11 +0800

    完成了单元的自动创建 (#2)
    Co-authored-by: hbc_thinkbook <419337244@qq.com>

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 from src.tool import BaiduOauthClient,ServiceTool
-from src.service import BaiduCampaignServiceClient
+from src.service import BaiduCampaignServiceClient,BaiduAdgroupServiceClient,BaiduCreativeServiceClient,BaiduKeywordServiceClient
 
 class BaiduBuildAds(object):
     def __init__(self,user_id:str,user_name:str):
         self.user_name = user_name
         self.user_id = user_id
         self.oauth_client = BaiduOauthClient(user_id)
-        self.campaign_map = {}
-        self.adgroup_map = {}
+        self.campaign_map = {'测试6': 776577181, 776577181: '测试6', '测试7': 776577180, 776577180: '测试7'}
+        self.adgroup_map = {776577181: {'单元测试有url': 11060930714, 11060930714: '单元测试有url', '单元测试无url': 11060930715, 11060930715: '单元测试无url', '单元测试pcurl': 11060930718, 11060930718: '单元测试pcurl', '单元测试mourl': 11060930719, 11060930719: '单元测试mourl', '单元测试无追踪代码': 11060930716, 11060930716: '单元测试无追踪代码', '单元测试代码匹配符': 11060930717, 11060930717: '单元测试代码匹配符'}, 776577180: {'单元测试出价': 11060930720, 11060930720: '单元测试出价'}}
         self.fail = {
             '计划':[],
             '单元':[],
@@ -28,15 +28,92 @@ class BaiduBuildAds(object):
                 self.campaign_map[item['campaignId']] = item['campaignName']
         print(f'\n失败记录:{self.fail}\n')
         print(f'\n计划映射表{self.campaign_map}\n')
+        
+    def _adgroup_fail_message(self,params,rsp_fail_msg:dict) -> dict:
+        fail_index = rsp_fail_msg['id']
+        param = params['adgroupTypes'][fail_index]
+        return {'失败位置':'新建单元','所属计划ID':param['campaignId'],'失败单元名称':param['adgroupName'],'失败原因':rsp_fail_msg['message']}
+    
+    def _update_adgroup_map(self,adgroup_info:dict)->None:
+        if self.adgroup_map.get(adgroup_info['campaignId']) is None:
+            self.adgroup_map[adgroup_info['campaignId']] = {}
+        self.adgroup_map[adgroup_info['campaignId']].update({adgroup_info['adgroupName']:adgroup_info['adgroupId']})
+        self.adgroup_map[adgroup_info['campaignId']].update({adgroup_info['adgroupId']:adgroup_info['adgroupName']})
+            
+    def create_adgroup(self,adgroup_item:list[dict]) -> dict:
+        adgroup_service = self.oauth_client.create_oauth_client(self.user_name,BaiduAdgroupServiceClient)
+        adgroup_params = ServiceTool().create_adgroup_params(adgroup_item,self.campaign_map)
+        for params in adgroup_params:
+            rsp = adgroup_service.add_adgroup(params)
+            if rsp['header']['desc'] != 'success':
+                for fail_msg in rsp['header']['failures']:
+                    self.fail['单元'].append(self._adgroup_fail_message(params,fail_msg))
+            if rsp['body']['data']:
+                for item in rsp['body']['data']:
+                    self._update_adgroup_map(item)
+            print(rsp)
+            print(f'\n失败记录:{self.fail}\n')
+            print(f'\n计划映射表{self.adgroup_map}\n')
+            
+            
 
 
 def main():
     uese_id = '64339991'
     user_name = '金蛛-北大青鸟'
     build_ads = BaiduBuildAds(uese_id,user_name)
-    campaign_name = ['测试2','测试5','测试3']
+    campaign_name = ['测试6','测试7','测试3']
     business_point_name = '品牌'
-    build_ads.create_campaign(campaign_name,business_point_name)
+    build_ads.create_campaign(campaign_name)
+    adgroup_list = [
+            {
+                    'campaignName':'测试6',
+                    "adgroupName":'单元测试有url',
+                    'pcFinalUrl':'https://www.jinzhuedu.org',
+                    'pcTrackParam':'?source=bd-pc&e_userid={userid}&e_planid={planid}&e_unitid={unitid}&e_keywordid={keywordid}&e_creative={creative}&e_matchtype={matchtype}&e_dongtai={dongtai}&e_trig_flag={trig_flag}&e_crowdid={crowdid}&e_kw_enc_utf8={kw_enc_utf8}',
+                    'mobileFinalUrl':'https://m.jinzhuedu.org',
+                    'mobileTrackParam':'?source=bd-mo&e_userid={userid}&e_planid={planid}&e_unitid={unitid}&e_keywordid={keywordid}&e_creative={creative}&e_matchtype={matchtype}&e_dongtai={dongtai}&e_trig_flag={trig_flag}&e_crowdid={crowdid}&e_kw_enc_utf8={kw_enc_utf8}',
+            },
+            {
+                    'campaignName':'测试6',
+                    "adgroupName":'单元测试无url',
+          },
+            {
+                    'campaignName':'测试6',
+                    "adgroupName":'单元测试pcurl',
+                    'pcFinalUrl':'https://www.jinzhuedu.org',
+                    'pcTrackParam':'?source=bd-pc&e_userid={userid}&e_planid={planid}&e_unitid={unitid}&e_keywordid={keywordid}&e_creative={creative}&e_matchtype={matchtype}&e_dongtai={dongtai}&e_trig_flag={trig_flag}&e_crowdid={crowdid}&e_kw_enc_utf8={kw_enc_utf8}',
+                    'mobileTrackParam':'?source=bd-mo&e_userid={userid}&e_planid={planid}&e_unitid={unitid}&e_keywordid={keywordid}&e_creative={creative}&e_matchtype={matchtype}&e_dongtai={dongtai}&e_trig_flag={trig_flag}&e_crowdid={crowdid}&e_kw_enc_utf8={kw_enc_utf8}',
+            },
+            {
+                    'campaignName':'测试6',
+                    "adgroupName":'单元测试mourl',
+                    'mobileFinalUrl':'https://www.jinzhuedu.org',
+                    'pcTrackParam':'&source=bd-pc&e_userid={userid}&e_planid={planid}&e_unitid={unitid}&e_keywordid={keywordid}&e_creative={creative}&e_matchtype={matchtype}&e_dongtai={dongtai}&e_trig_flag={trig_flag}&e_crowdid={crowdid}&e_kw_enc_utf8={kw_enc_utf8}',
+                    'mobileTrackParam':'&source=bd-mo&e_userid={userid}&e_planid={planid}&e_unitid={unitid}&e_keywordid={keywordid}&e_creative={creative}&e_matchtype={matchtype}&e_dongtai={dongtai}&e_trig_flag={trig_flag}&e_crowdid={crowdid}&e_kw_enc_utf8={kw_enc_utf8}',
+            },
+            {
+                    'campaignName':'测试6',
+                    "adgroupName":'单元测试无追踪代码',
+                    'pcFinalUrl':'https://www.jinzhuedu.org',
+                    'mobileFinalUrl':'https://www.jinzhuedu.org',
+          },
+            {
+                    'campaignName':'测试6',
+                    "adgroupName":'单元测试代码匹配符',
+                    'pcFinalUrl':'https://www.jinzhuedu.org?123',
+                    'mobileFinalUrl':'https://www.jinzhuedu.org?123',
+          },
+            {
+                    'campaignName':'测试7',
+                    "adgroupName":'单元测试出价',
+                    'pcFinalUrl':'https://www.jinzhuedu.org',
+                    'mobileFinalUrl':'https://www.jinzhuedu.org',
+                    'maxPrice':0.3
+          }      
+    ]
+    build_ads.create_adgroup(adgroup_list)
+    
 
 if __name__ == '__main__':
     main()
--- src/tool/baidu_service_tool.py
+++ src/tool/baidu_service_tool.py
@@ -6,6 +6,14 @@ class ServiceTool(object):
             '品牌':99,
             '软件开发':201904057005
         }
+    @staticmethod
+    def chunk_list(lst, size=100):
+        """将列表按指定大小分块
+        :param lst: 原始列表
+        :param size: 每块大小（默认100）
+        :return: 分块后的二维列表
+        """
+        return [lst[i:i+size] for i in range(0, len(lst), size)]
     def _campaign_params(self,campaign_name:str,business_point_name:str|None) -> dict:
             if business_point_name is None:
                   business_point_name = '软件开发'
@@ -24,11 +32,100 @@ class ServiceTool(object):
          
     def create_campaign_params(self,campaign_name:str,business_point_name:str|None) -> dict:
 
-            if isinstance(campaign_name,str):
-                campaign_params = [self._campaign_params(campaign_name,business_point_name)]
+        if isinstance(campaign_name,str):
+            campaign_params = [self._campaign_params(campaign_name,business_point_name)]
 
-            if isinstance(campaign_name,list):
-                campaign_params = [self._campaign_params(item,business_point_name) for item in campaign_name]
+        if isinstance(campaign_name,list):
+            campaign_params = [self._campaign_params(item,business_point_name) for item in campaign_name]
             
-            return {'campaignTypes':campaign_params}
+        return {'campaignTypes':campaign_params}
+    def is_empty(self,value:any,empty_value:list|None = ['',[],{}]):
+         
+        if empty_value is None:
+            if value is None:
+                return True
+            else:
+                return False
+        for empty_item in empty_value:
+            if value == empty_item:
+                return True
+        return False
+
+    def _set_adgroup_params(self,adgroup_item:dict,campaign_map:dict|None = None) -> dict:
+        params = {}
+        for key,value in adgroup_item.items():
+            if key == 'campaignName':
+                if campaign_map is None:
+                    raise Exception('请传入计划id')
+                else:
+                    campaign_id = campaign_map.get(value)
+                    if campaign_id is None:
+                        raise Exception(f'计划{value},没有对应的campaignId')
+                    params['campaignId'] = campaign_id
+                    continue
+                
+            if key == 'adgroupName':
+                params[key] = value
+                continue
+            
+            if key == 'maxPrice':
+                params[key] = value or 0.3
+                continue
+            
+            if key == 'pcFinalUrl':
+                if self.is_empty(value):
+                    pass
+                else:
+                    params[key] = value
+                continue
+            if key == 'mobileFinalUrl':
+                if self.is_empty(value):
+                    pass
+                else:
+                    params[key] = value
+                continue
+            if key == 'pcTrackParam':
+                if self.is_empty(value):
+                    params[key] = 'source=bd-pc&e_userid={userid}&e_planid={planid}&e_unitid={unitid}&e_keywordid={keywordid}&e_creative={creative}&e_matchtype={matchtype}&e_dongtai={dongtai}&e_trig_flag={trig_flag}&e_crowdid={crowdid}&e_kw_enc_utf8={kw_enc_utf8}&e_bd_vid={bd_vid}'
+                else:
+                    params[key] = value
+                continue
+            if key == 'mobileTrackParam':
+                if self.is_empty(value):
+                    params[key] = 'source=bd-mo&e_userid={userid}&e_planid={planid}&e_unitid={unitid}&e_keywordid={keywordid}&e_creative={creative}&e_matchtype={matchtype}&e_dongtai={dongtai}&e_trig_flag={trig_flag}&e_crowdid={crowdid}&e_kw_enc_utf8={kw_enc_utf8}&e_bd_vid={bd_vid}'
+                else:
+                    params[key] = value
+                continue
+        if params.get('pcTrackParam') is None:
+            params['pcTrackParam'] =  'source=bd-pc&e_userid={userid}&e_planid={planid}&e_unitid={unitid}&e_keywordid={keywordid}&e_creative={creative}&e_matchtype={matchtype}&e_dongtai={dongtai}&e_trig_flag={trig_flag}&e_crowdid={crowdid}&e_kw_enc_utf8={kw_enc_utf8}&e_bd_vid={bd_vid}'
+        if params.get('mobileTrackParam') is None:
+            params['mobileTrackParam'] = 'source=bd-mo&e_userid={userid}&e_planid={planid}&e_unitid={unitid}&e_keywordid={keywordid}&e_creative={creative}&e_matchtype={matchtype}&e_dongtai={dongtai}&e_trig_flag={trig_flag}&e_crowdid={crowdid}&e_kw_enc_utf8={kw_enc_utf8}&e_bd_vid={bd_vid}'
+        
+        if params['pcTrackParam'][0] in ['?','&']:
+            params['pcTrackParam'] = params['pcTrackParam'][1:]
+        if params['mobileTrackParam'][0] in ['?','&']:
+            params['mobileTrackParam'] = params['mobileTrackParam'][1:]
+            
+        if params.get('pcFinalUrl') is None:
+            if params.get('pcTrackParam'):
+                del params['pcTrackParam']
+        else:
+            if '?' in params['pcFinalUrl']:
+                params['pcTrackParam'] = '&' + params['pcTrackParam']
+            else:
+                params['pcTrackParam'] = '?' + params['pcTrackParam']
+        if params.get('mobileFinalUrl') is None:
+            if params.get('mobileTrackParam'):
+                del params['mobileTrackParam']
+        else:
+            if '?' in params['mobileFinalUrl']:
+                params['mobileTrackParam'] = '&' + params['mobileTrackParam']
+            else:
+                params['mobileTrackParam'] = '?' + params['mobileTrackParam']
+        return params
+    def create_adgroup_params(self,adgroup_item,campaign_map) -> dict:
+        result = []
+        adgroup_params = [self._set_adgroup_params(item,campaign_map) for item in adgroup_item]
+        params_list = self.chunk_list(adgroup_params)
+        return [{'adgroupTypes':params} for params in params_list]
 
